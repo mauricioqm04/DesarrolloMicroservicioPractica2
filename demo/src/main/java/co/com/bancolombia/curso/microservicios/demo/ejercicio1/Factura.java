5833a7116aa45f0f92516381a4bc5cb6f011a23c
mauricioqm04 <105473296+mauricioqm04@users.noreply.github.com>
2026-06-03 08:54:50 -0500

    Add files via upload

--- demo/src/main/java/co/com/bancolombia/curso/microservicios/demo/ejercicio1/Factura.java
+++ demo/src/main/java/co/com/bancolombia/curso/microservicios/demo/ejercicio1/Factura.java
@@ -1,4 +1,66 @@
 package co.com.bancolombia.curso.microservicios.demo.ejercicio1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Factura {
+
+    private String numeroFactura;
+    private String cliente;
+    private List<Pagable> items;
+
+    public Factura(String numeroFactura, String cliente) {
+        this.numeroFactura = numeroFactura;
+        this.cliente = cliente;
+        this.items = new ArrayList<>();
+    }
+
+    public void agregarItem(Pagable item) {
+        this.items.add(item);
+        System.out.println("Item agregado: " + item.descripcion());
+    }
+
+    public double calcularGranTotal() {
+        double granTotal = (double) 0.0F;
+        for (Pagable item : items) {
+            granTotal += item.calcularPago();
+        }
+        return granTotal;
+    }
+
+    public void imprimirFactura(){
+        System.out.println("\n|------------------------------|");
+        System.out.println("|           FACTURA            |");
+        System.out.println("|------------------------------|");
+        System.out.println("\nNúmero de Factura: " + this.numeroFactura);
+        System.out.println("Cliente: " + this.cliente);
+        System.out.println("Items:");
+        for (Pagable item : items) {
+            System.out.println("- " + item.descripcion() + ": $" + item.calcularPago());
+        }
+        System.out.println("\nTotal a pagar: $" + this.calcularGranTotal());
+    }
+
+    public void serializarItems(){
+        System.out.println("\nSerializando items de la factura...");
+        for (Pagable item : this.items) {
+            if(item instanceof Serializable){
+                System.out.println(" "+ ((Serializable)item).serializar());
+            }
+        }
+    }
+
+    public List<Pagable> getItems() {
+        return this.items;
+    }
+
+    public String getNumeroFactura() {
+        return this.numeroFactura;
+    }
+
+    public String getCliente() {
+        return this.cliente;
+    }
+
+
 }
--- demo/src/main/java/co/com/bancolombia/curso/microservicios/demo/ejercicio1/Main.java
+++ demo/src/main/java/co/com/bancolombia/curso/microservicios/demo/ejercicio1/Main.java
@@ -1,4 +1,33 @@
 package co.com.bancolombia.curso.microservicios.demo.ejercicio1;
 
 public class Main {
+
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 1: Factura con polimorfismo y Serialización\n");
+        Producto laptop = new Producto("Laptop", 1500.00, 2);
+        Producto teclado = new Producto("Teclado", 1200.00, 4);
+        Producto monitor = new Producto("Monitor", 5200.00, 5);
+        Producto soporte = new Producto("Soporte", 300.00, 2);
+        Producto desarrollo = new Producto("Desarrollo software", 500.00, 10);
+
+        Factura factura = new Factura("F001", "Juan Pérez");
+        factura.agregarItem(laptop);
+        factura.agregarItem(teclado);
+        factura.agregarItem(monitor);
+        factura.agregarItem(soporte);
+        factura.agregarItem(desarrollo);
+        factura.imprimirFactura();
+        factura.serializarItems();
+
+
+        Pagable [] elementos = new Pagable[]{laptop,teclado,monitor,soporte,desarrollo};
+
+        for(Pagable elemento : elementos){
+            System.out.println("\nDescripcion: "+elemento.descripcion());
+            System.out.println("Pago a realizar: "+ elemento.calcularPago());
+        }
+    }
 }
